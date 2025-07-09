0f435ab2314c06ba8b7af85df8b7b86400e5fc36
Goga-ton <goga_ton@mail.ru>
2025-07-09 15:44:56 +0300

    add commit

--- Test.py
+++ Test.py
@@ -1,9 +1,4 @@
+import datetime
 
-a = 1
-b = 3
-c = 5
-d = input('input number')
-if d == a or d ==b or d ==c:
-    print('есть совпадение')
-else:
-    print('совпадений не найдено')
+now = datetime.datetime.now().strftime('%Y.%M %H:%M')
+print(now)
